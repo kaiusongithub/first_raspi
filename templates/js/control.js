eea55ffaa3e7171afd5227d4b0f321af5a3e0b85
kaiusongithub <kai.osterhusgmx.de>
2018-10-08 14:23:38 +0200

    adding a new js file

---
+++ templates/js/control.js
@@ -0,0 +1,6 @@
+var app = angular.module('myApp', []);
+
+/*$location is a service which returns information about the location of a web page*/
+app.controller('myCtrl', function($scope) {
+    $scope.serialNumber= "0";
+});
