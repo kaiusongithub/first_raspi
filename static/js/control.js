14fff0181528ed7bffa6bf457c41c48a3370b79d
kaiusongithub <kai.osterhusgmx.de>
2018-10-09 11:46:51 +0200

    added a comment for test reasons

--- static/js/control.js
+++ static/js/control.js
@@ -1,6 +1,7 @@
 var app = angular.module('myApp', ['ngMaterial']);
 
 /*$location is a service which returns information about the location of a web page*/
+// check if something happens
 app.controller('myCtrl', function($scope) {
 
 });
