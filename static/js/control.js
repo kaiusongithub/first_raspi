b00ae99b8b9b3e02ebaed20886b199194e773d42
kaiusongithub <kai.osterhusgmx.de>
2018-10-09 17:16:33 +0200

    changed json structure

--- static/js/control.js
+++ static/js/control.js
@@ -3,10 +3,7 @@ var app = angular.module('myApp', ['ngMaterial']);
 /*$location is a service which returns information about the location of a web page*/
 // check if something happens
 app.controller('myCtrl', function($scope) {
-	$scope.rooms = [
-		{"key": "living", "name": "Wohnzimmer", "id": "icon_living_room"},
-		{"key": "kitchen", "name": "Küche", "id": "icon_kitchen"},
-		{"key": "bed", "name": "Schlafzimmer", "id": "icon_bed_room"},
-		{"key": "office", "name": "Büro", "id": "icon_office"}
-	];
+	$scope.rooms = 
+		{"key": "living", "name": "Wohnzimmer"}
+		{"key": "kitchen", "name": "Küche"};
 });
