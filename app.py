7c93d50a453b37354c8ab8becfda2c23de5181b2
kaiusongithub <kai.osterhusgmx.de>
2018-10-09 15:12:43 +0200

    added a new route to a rooms details page

--- app.py
+++ app.py
@@ -14,5 +14,9 @@ def kaius():
 def control():
 	return render_template('control.html')
 
+@app.route('/control/room/<room>')
+def room(room):
+	return render_template(room.html, room=room)
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
