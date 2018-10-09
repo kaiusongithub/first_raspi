0a0f0ecd023e747a3f982c051f2ffc3994285fd7
kaiusongithub <kai.osterhusgmx.de>
2018-10-09 16:42:33 +0200

    getting rooms via json

--- static/js/control.js
+++ static/js/control.js
@@ -3,5 +3,10 @@ var app = angular.module('myApp', ['ngMaterial']);
 /*$location is a service which returns information about the location of a web page*/
 // check if something happens
 app.controller('myCtrl', function($scope) {
-
+	$scope.rooms = [
+		{ key: 'living', name: 'Wohnzimmer', id: 'icon_living_room' },
+		{ key: 'kitchen', name: 'Küche', id: 'icon_kitchen' },
+		{ key: 'bed', name: 'Schlafzimmer', id: 'icon_bed_room' },
+		{ key: 'office', name: 'Büro', id: 'icon_office' }
+	];
 });
