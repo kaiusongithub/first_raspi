3024f75baad1196febbb0abfefe8346b6dc5f0ca
kaiusongithub <kai.osterhusgmx.de>
2018-10-09 11:38:45 +0200

    snytax changes

--- static/js/control.js
+++ static/js/control.js
@@ -1,6 +1,6 @@
-var app = angular.module('myApp', ['ngMaterial']);
+var app = angular.module('myApp', ['ngMaterial', 'ngMessages']);
 
 /*$location is a service which returns information about the location of a web page*/
 app.controller('myCtrl', function($scope) {
-	
+
 });
