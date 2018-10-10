283cd5e6e47c4f5b7c2d9a2d50360bf191ed3e4b
kaiusongithub <kai.osterhusgmx.de>
2018-10-10 09:57:21 +0200

    display  as Room name

--- static/js/control.js
+++ static/js/control.js
@@ -26,5 +26,5 @@ app.controller('myCtrl', function($scope, $route, $routeParams) {
 
  app.controller('roomCtrl', function($scope, $routeParams) {
     // $scope.selectedroom = $routeParams.room;
-    $scope.selectedroom = "Random";
+    $scope.selectedroom = $routeParams;
  });
