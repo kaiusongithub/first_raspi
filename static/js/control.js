debde6c58a67b77aec2afe28942f22c26ff4d942
kaiusongithub <kai.osterhusgmx.de>
2018-10-10 17:49:21 +0200

    fix data type issues

--- static/js/control.js
+++ static/js/control.js
@@ -74,7 +74,7 @@ app.controller('myCtrl', function($scope, $mdSidenav, $http) {
     });
 
     // update weather based on selected country
-    $scope.userCity = 49413;
+    $scope.userCity = "49413,de";
     $scope.updateWeather = function() {
     	$http.get("http://api.openweathermap.org/data/2.5/weather?zip=" + $scope.userCity + "&APPID=4a97ae0f4ff2538a214cf773a5351ff4&units=metric")
     	.then(function(response) {
