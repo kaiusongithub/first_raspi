883b5ea2228f7a31601ed2963b79515307bad4d1
kaiusongithub <kai.osterhusgmx.de>
2018-10-10 20:28:52 +0200

    playing around

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ def control():
 	for row in cur.fetchall() :
 		roomKey = str(row[0])
 		roomName = str(row[1])
-    return roomKey
-    return roomName
+	return roomKey
+	return roomName
 	return render_template('control.html')
 
 @app.route('/getRooms', methods=['GET'])
