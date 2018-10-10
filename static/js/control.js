7204ce82260824468a84a053e9b9c2af20ead38c
kaiusongithub <kai.osterhusgmx.de>
2018-10-10 10:00:06 +0200

    trying to get routeParams

--- static/js/control.js
+++ static/js/control.js
@@ -4,24 +4,19 @@ var app = angular.module('myApp', ['ngMaterial', 'ngRoute']);
 // get the URL parameters based on the route
 app.config(function($routeProvider) {
   $routeProvider
-  .when('/room/:selectedroom', {
+  .when('/control/room/:selectedroom', {
     templateUrl : "room.html",
     controller: 'roomCtrl'
   });
 });
 
-app.controller('myCtrl', function($scope, $route, $routeParams) {
+app.controller('myCtrl', function($scope) {
 	$scope.rooms = [
 		{"key": "livingroom", "name": "Wohnzimmer", "divid": "icon_living_room"},
 		{"key": "kitchen", "name": "Küche", "divid": "icon_kitchen"},
 		{"key": "bedroom", "name": "Schlafzimmer", "divid": "icon_bed_room"},
 		{"key": "office", "name": "Büro", "divid": "icon_office"}
 		];
-
-	// getting the selected room from the URL
-	$scope.$route = $route;
-    $scope.$routeParams = $routeParams;
-    $scope.selectedroom = "random";
 });
 
  app.controller('roomCtrl', function($scope, $routeParams) {
