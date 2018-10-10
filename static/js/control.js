33cb6f8baa0e4da02bc8b159fdf5e30f08fbea55
kaiusongithub <kai.osterhusgmx.de>
2018-10-10 12:27:45 +0200

    remove routeParams from app config

--- static/js/control.js
+++ static/js/control.js
@@ -2,7 +2,7 @@
 var app = angular.module('myApp', ['ngMaterial', 'ngRoute']);
 
 // get the URL parameters based on the route
-app.config(function($routeProvider, $locationProvider, $routeParams) {
+app.config(function($routeProvider, $locationProvider) {
   $routeProvider
   .when('/rooms', {
     controller: 'myCtrl'
