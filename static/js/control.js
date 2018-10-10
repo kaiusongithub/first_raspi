939a5448034d1b8b97a9a39b2f1fcb40452f78bc
kaiusongithub <kai.osterhusgmx.de>
2018-10-10 08:52:23 +0200

    add  functionality

--- static/js/control.js
+++ static/js/control.js
@@ -1,12 +1,32 @@
-var app = angular.module('myApp', ['ngMaterial']);
+var app = angular.module('myApp', ['ngMaterial'], ['ngRoute']);
 
-/*$location is a service which returns information about the location of a web page*/
-// check if something happens
-app.controller('myCtrl', function($scope) {
+// get the URL parameters based on the route
+.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+  .when('/control/room/:room', {
+    templateUrl: 'room.html',
+    controller: 'roomCtrl'
+  });
+
+  // configure html5 to get links working on jsfiddle
+  $locationProvider.html5Mode(true);
+});
+
+app.controller('myCtrl', function($scope, $route, $routeParams, $location) {
 	$scope.rooms = [
 		{"key": "livingroom", "name": "Wohnzimmer", "divid": "icon_living_room"},
 		{"key": "kitchen", "name": "Küche", "divid": "icon_kitchen"},
 		{"key": "bedroom", "name": "Schlafzimmer", "divid": "icon_bed_room"},
 		{"key": "office", "name": "Büro", "divid": "icon_office"}
 		];
+
+	// getting the selected room from the URL
+	$scope.$route = $route;
+    $scope.$location = $location;
+    $scope.$routeParams = $routeParams;
+    $scope.params = {};
 });
+
+ app.controller('roomCtrl', function($scope, $routeParams) {
+     $scope.params = $routeParams;
+ });
