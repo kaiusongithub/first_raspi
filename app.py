96c9ec44db89a5b1c1057ada479dd7d58f789c7f
kaiusongithub <kai.osterhusgmx.de>
2018-10-10 11:29:09 +0200

    fixed app.py

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('rooms')
+@app.route('/rooms')
 def control():
 	return render_template('control.html')
 
-@app.route('rooms/<selectedroom>')
+@app.route('/rooms/<selectedroom>')
 def room(selectedroom):
 	return render_template('room.html', selectedroom=room)
 
