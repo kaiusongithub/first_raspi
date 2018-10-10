b7296ecd138307be189d6507a8363a0bbae3e93e
kaiusongithub <kai.osterhusgmx.de>
2018-10-10 09:36:35 +0200

    trying to remove raw and endraw

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def control():
 	return render_template('control.html')
 
-# @app.route('/control/room/<room>')
-# def room(room):
-# 	return render_template('room.html', room=room)
+@app.route('/control/room/<room>')
+def room(room):
+	return render_template('room.html', room=room)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
